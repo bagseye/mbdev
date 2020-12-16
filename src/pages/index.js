e6f4daedc7ffecb9e59f84d3eedfde57e14270d3
Morgan Baker <morganbakerdesigner@gmail.com>
2020-12-16 19:27:52 +0000

    Remove front image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Banner from '../components/Banner/Banner';
-import About from '../components/About/About';
-import SEO from '../components/SEO';
-import FullWidthImage from '../components/FullWidthImage/FullWidthImage';
-import FeatureDevs from '../components/FeatureDevelopments';
-import Layout from '../components/Layout';
-import Journal from '../components/Journal';
-import Grid from '../components/Grid';
-import ContactMethods from '../components/ContactMethods';
-import { SplitContainerStyles } from '../styles/SiteStyles';
+import React from "react";
+import Banner from "../components/Banner/Banner";
+import About from "../components/About/About";
+import SEO from "../components/SEO";
+import FullWidthImage from "../components/FullWidthImage/FullWidthImage";
+import FeatureDevs from "../components/FeatureDevelopments";
+import Layout from "../components/Layout";
+import Journal from "../components/Journal";
+import Grid from "../components/Grid";
+import ContactMethods from "../components/ContactMethods";
+import { SplitContainerStyles } from "../styles/SiteStyles";
 
 const Index = () => (
   <Layout>
     <SEO title="Home" />
     <Banner description="I'm a web developer & designer from Inverness, building fast, stylish websites using WordPress & GatsbyJS" />
-    <FullWidthImage />
+    {/* <FullWidthImage /> */}
     <About id="about" />
     <FeatureDevs />
     <SplitContainerStyles>
