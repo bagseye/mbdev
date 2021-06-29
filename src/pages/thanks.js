6042b065331f59e2add67ba3b8c4da8758d32b21
Morgan Baker <morganbakerdesigner@gmail.com>
2021-06-29 23:33:48 +0100

    Changed thank you page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Banner from "../components/Banner/Banner";
 
 const Thanks = () => {
   return (
     <Layout>
-      <h1>Thank you!</h1>
-      <p>This is a custom thank you page for form submissions</p>
+      <Banner heading="Thank you" />
     </Layout>
   );
 };
