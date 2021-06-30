b59cdb31bff1fcfe8ad3be1c2af3ad2364eb2989
Morgan Baker <morganbakerdesigner@gmail.com>
2021-06-30 19:47:36 +0100

    New quote form

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,53 +1,14 @@
 import React from "react";
-import { navigate } from "gatsby-link";
 import { QuoteStyles } from "./QuoteStyles.js";
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
-
 const Quote = () => {
-  const [state, setState] = React.useState({});
-
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encodeURI({
-        "form-name": form.getAttribute("name"),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch((error) => alert(error));
-  };
   return (
     <QuoteStyles>
-      <form
-        name="quote"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
+      <form name="quote" method="post" action="/thanks/" data-netlify="true">
         <input type="hidden" name="form-name" value="quote" />
         <div className="area">
           <label>Your name </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            onChange={handleChange}
-          />
+          <input type="text" name="name" placeholder="Enter your name" />
         </div>
         <div className="area">
           <label>Your business name </label>
@@ -55,7 +16,6 @@ const Quote = () => {
             type="text"
             name="business"
             placeholder="Enter your business name"
-            onChange={handleChange}
           />
         </div>
         <div className="area">
@@ -64,26 +24,15 @@ const Quote = () => {
             type="text"
             name="website"
             placeholder="Enter your web address"
-            onChange={handleChange}
           />
         </div>
         <div className="area">
           <label>Email contact </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email address"
-            onChange={handleChange}
-          />
+          <input type="email" name="email" placeholder="Email address" />
         </div>
         <div className="area">
           <label>Phone number </label>
-          <input
-            type="number"
-            name="phone"
-            placeholder="Phone number"
-            onChange={handleChange}
-          />
+          <input type="number" name="phone" placeholder="Phone number" />
         </div>
         <div className="area">
           <label>Any requirements needed for your new site? </label>
@@ -91,7 +40,6 @@ const Quote = () => {
             type="text"
             name="requirements"
             placeholder="Write your requirements here"
-            onChange={handleChange}
           />
         </div>
 
