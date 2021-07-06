7ceef6352719607b7bfb0737f514926efa118da8
Morgan Baker <morganbakerdesigner@gmail.com>
2021-07-06 08:10:20 +0100

    Add new gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,20 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-020XNGJ953", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Morgan Baker Developer`,
