d809708c50bb13da5b70ae45bcae23207228c199
Morgan Baker <morganbakerdesigner@gmail.com>
2021-11-26 23:19:03 +0000

    Added header video

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { BsChevronDown as Scroller } from "react-icons/bs";
+import { BsChevronDown as Scroller, BsStarFill } from "react-icons/bs";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { motion } from "framer-motion";
 import PropTypes from "prop-types";
+import videoPlaceholder from "../../../static/wave-still.jpg";
+import desktopVideo from "../../../static/wave-desktop.mp4";
+import tabletVideo from "../../../static/wave-mobile.mp4";
+import mobileVideo from "../../../static/wave-mobile.mp4";
 
 const BannerGridColumn = styled.section`
   grid-column: 1 / 7;
@@ -56,10 +60,62 @@ const BannerStyles = styled.div`
       object-position: center;
     }
   }
+
+  .banner__video--placeholder {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    filter: blur(6px);
+    transition: opacity 0.5s;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
+
+  .banner__video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    transition: opacity 0.5s;
+
+    video {
+      object-fit: cover;
+      object-position: center;
+      width: 100%;
+      height: 100%;
+      opacity: 0.25;
+      filter: blur(6px);
+    }
+  }
 `;
 
 const Banner = ({ bannerContent, scrollerText, image }) => {
   const bannerImage = getImage(image);
+  const placeholderImage = getImage(videoPlaceholder);
+
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  const onLoadedData = () => {
+    setIsVideoLoaded(true);
+  };
+
+  const getVidSrc = (width) => {
+    if (width >= 1080) return desktopVideo;
+    if (width >= 720) return tabletVideo;
+    return mobileVideo;
+  };
+
+  const vidSrc = getVidSrc(window.innerWidth);
 
   return (
     <BannerStyles styles={{ backgroundColor: image ? "#000" : "" }}>
@@ -96,6 +152,22 @@ const Banner = ({ bannerContent, scrollerText, image }) => {
       </div>
       {bannerImage && (
         <GatsbyImage className="banner__bg" image={bannerImage} />
+      )}
+      {!bannerImage && (
+        <>
+          <GatsbyImage
+            className="banner__video--placeholder"
+            image={placeholderImage}
+            style={{ opacity: isVideoLoaded ? 0 : 1 }}
+          />
+          <div
+            className="banner__video"
+            onLoadedData={onLoadedData}
+            style={{ opacity: isVideoLoaded ? 1 : 0 }}
+          >
+            <video autoPlay loop muted playsInline src={vidSrc}></video>
+          </div>
+        </>
       )}
     </BannerStyles>
   );
