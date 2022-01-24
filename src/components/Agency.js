07b822370866b0caa4351ed09a17cb45b6b773f3
Morgan Baker <morganbakerdesigner@gmail.com>
2022-01-24 16:48:08 +0000

    Added tech used

--- src/components/Agency.js
+++ src/components/Agency.js
@@ -6,6 +6,30 @@ import Layout from "../components/Layout";
 import ProjectNavigation from "./ProjectNavigation";
 import Banner from "./Banner/Banner";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import styled from "styled-components";
+
+const TechUsed = styled.div`
+  margin-top: calc(var(--gridGap) * 3);
+  margin-bottom: calc(var(--gridGap) * 3);
+
+  .col {
+    &:nth-child(1) {
+      grid-column: 1 / 7;
+
+      @media (min-width: 834px) {
+        grid-column: 1 / 3;
+      }
+    }
+
+    &:nth-child(2) {
+      grid-column: 1 / 7;
+
+      @media (min-width: 834px) {
+        grid-column: 3 / 5;
+      }
+    }
+  }
+`;
 
 const Agency = ({ data, next }) => {
   let nextItem = next !== undefined ? next.node : null;
@@ -13,6 +37,8 @@ const Agency = ({ data, next }) => {
   const {
     name,
     excerpt,
+    website,
+    technology,
     richDescription,
     richDescription: { raw },
     images,
@@ -33,6 +59,29 @@ const Agency = ({ data, next }) => {
         >
           <GatsbyImage className="banner__bg" image={mainImage} />
         </Banner>
+        <TechUsed>
+          <div className="container container-grid">
+            {technology && (
+              <div className="col">
+                <p>
+                  <strong>Built using: </strong>
+                  {technology}
+                </p>
+              </div>
+            )}
+
+            {website && (
+              <div className="col">
+                <p>
+                  <strong>Website: </strong>
+                  <a href={`https://${website}`} target="_blank">
+                    {website}
+                  </a>
+                </p>
+              </div>
+            )}
+          </div>
+        </TechUsed>
         <GeneralContent content={richDescription} />
         <Gallery projectImages={projectImages} />
         <ProjectNavigation next={nextItem} prefix="dashboard" />
