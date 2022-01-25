965a797841605f6b014dfc0a2d1348833912a7a6
Morgan Baker <morganbakerdesigner@gmail.com>
2022-01-25 23:19:27 +0000

    Restyled blog posts

---
+++ src/components/Posts/PostsGlobalStyles.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+export const PostContainerGlobalStyles = styled.div`
+  display: flex;
+  gap: 0 var(--gridGap);
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  @media (min-width: 600px) {
+    overflow-x: visible;
+    scroll-snap-type: none;
+  }
+
+  &.noscroll {
+    overflow-x: visible;
+    flex-wrap: wrap;
+    gap: var(--gridGap);
+
+    > a {
+      width: 100%;
+
+      @media (min-width: 600px) {
+        width: calc(50% - calc(var(--gridGap) / 2));
+      }
+
+      @media (min-width: 768px) {
+        width: calc(33.333% - 14px);
+      }
+
+      @media (min-width: 834px) {
+        width: calc(33.333% - 20px);
+      }
+    }
+  }
+`;
+
+export const PostGlobalStyles = styled(Link)`
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 5px;
+  width: calc(100% - 50px);
+  flex-shrink: 0;
+  padding: var(--gridGap);
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  transition: border-color 0.3s;
+  scroll-snap-align: center;
+
+  @media (min-width: 600px) {
+    width: 33.333%;
+    flex-shrink: 1;
+  }
+
+  .route {
+    color: var(--secondary);
+  }
+
+  span {
+    margin-top: auto;
+  }
+
+  > * {
+    &:first-child {
+      margin-top: 0;
+    }
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  &:hover {
+    border-color: var(--accent);
+  }
+`;
--- src/pages/journal.js
+++ src/pages/journal.js
@@ -2,39 +2,31 @@ import React from "react";
 import Layout from "../components/Layout";
 import useAllStories from "../hooks/use-all-stories";
 import TagItems from "../components/TagItems/TagItems";
-import { JournalListStyles } from "../components/Journal/JournalGlobalStyles";
-import JournalItem from "../components/Journal/JournalItem";
+import PostContainer from "../components/Posts/PostContainer";
+import Post from "../components/Posts/Post";
 
 const journalsPage = () => {
   const allStories = useAllStories();
 
   return (
     <Layout>
-      <JournalListStyles className="topgap">
-        <div className="container">
-          <div className="intro__area">
-            <h1>Web development journal and open-source project updates</h1>
-            <h4>
-              A deep-dive into my progress in web development, with a focus on
-              WordPress, React, Gatsby and open-source.
-            </h4>
-          </div>
-          <TagItems />
-          <div className="journal__content">
-            {allStories.map((node, index) => {
-              return (
-                <JournalItem
-                  key={index}
-                  title={node.title}
-                  path={node.gatsbyPath}
-                  createdAt={node.createdAt}
-                  tagsMeta={node.metadata.tags}
-                />
-              );
-            })}
-          </div>
+      <div className="container topgap">
+        <div className="intro__area">
+          <h1>Web development journal and open-source project updates</h1>
+          <h4>
+            A deep-dive into my progress in web development, with a focus on
+            WordPress, React, Gatsby and open-source.
+          </h4>
         </div>
-      </JournalListStyles>
+      </div>
+      <div className="container">
+        <TagItems />
+      </div>
+      <PostContainer noscroll={true}>
+        {allStories.map((node) => {
+          return <Post node={node} />;
+        })}
+      </PostContainer>
     </Layout>
   );
 };
