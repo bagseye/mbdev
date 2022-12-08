7f1c16027c1c01d2c0f857f77215b1339d9f1833
Bluestone - Morgan <morgan@bluestone98.com>
2022-12-08 00:39:27 +0000

    Update ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { MenuProvider } from "./src/components/MenuContext";
 import { AnimatePresence } from "framer-motion";
+
+class SessionCheck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+
+  handleCheckSession = () => {
+    this.setState({ loading: false });
+  };
+
+  componentDidMount() {
+    silentAuth(this.handleCheckSession);
+  }
+
+  render() {
+    return (
+      this.state.loading === false && (
+        <React.Fragment>{this.props.children}</React.Fragment>
+      )
+    );
+  }
+}
+
+export function wrapRootElement({ element }) {
+  return (
+    <SessionCheck>
+      <MenuProvider>{element}</MenuProvider>
+    </SessionCheck>
+  );
+}
+
 
 export function wrapPageElement({ element }) {
   return <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>;
