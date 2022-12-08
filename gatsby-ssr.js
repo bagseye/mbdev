8bb4d2be30f0f074c12553917377da132cd5c3d0
Bluestone - Morgan <morgan@bluestone98.com>
2022-12-08 00:34:22 +0000

    Updated ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { AnimatePresence } from "framer-motion";
+
+export function wrapPageElement({ element }) {
+  return <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>;
+}
